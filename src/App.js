0743c0eaf121179cd884bbe992851150cfc45754
Sanket Shetty <legendhimself007@gmail.com>
2024-08-05 23:25:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+ 
+const [amount, setAmount] = useState(1)
+const [fromCurrency, setFromCurrency] = useState("USD")
+const [toCurrency, setToCurrency] = useState("INR")
+const [exchangeRate, setExchangeRate] = useState({})
+const [convertedAmount, setConvertedAmount] = useState(null)
+
+
+const handleChange = (e) =>{
+const { name, value} = e.target;
+
+switch (name) {
+  case "amount":
+    setAmount(value)
+    break;
+
+  case "fromCurrency":
+    setFromCurrency(value)
+    break;
+
+  case "toCurrency":
+    setToCurrency(value)
+    break;
+}
+}
+
+
+useEffect(() => {
+
+  const apiUrl = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+
+  axios.get(apiUrl)
+  .then(
+    response =>{
+      setExchangeRate(response.data.rates);
+    }
+  )
+  .catch(
+    error=>{
+      console.error("The currency fetch was failed due to an error :", error);
+    }
+  )
+
+}, [fromCurrency])
+
+
+useEffect(() => {
+ const conversionRate = exchangeRate[toCurrency]
+
+if (conversionRate){
+  const converted = amount * conversionRate;
+  setConvertedAmount(converted.toFixed(2))
+}
+}, [amount,fromCurrency, toCurrency, exchangeRate])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+    <img className="imgs" src="https://i.gifer.com/origin/0d/0dd77a5f8480e6a51439776f04f317e8_w200.gif" alt="" />
+      <h1 className="text-6xl">Currency Converter</h1>
+
+      <div className="currency_exchnage">
+        <div className="input_container">
+          <label className="input_label">Amount:</label>
+          <input
+            type="number"
+            name="amount"
+            value={amount}
+            onChange={handleChange}
+            className="input_field"
+          />
+        </div>
+        <div className="input_container">
+          <label className="input_label">From:</label>
+          <select
+            name="fromCurrency"
+          value={fromCurrency}
+          onChange={handleChange}
+            className="input_field"
+          >
+          {Object.keys(exchangeRate).map(i=>(
+            <option key={i} value={i}>{i}</option>
+          ))}
+          </select>
+        </div>
+        <div className="input_container">
+          <label className="input_label">To:</label>
+          <select
+            name="toCurrency"
+          value={toCurrency}
+          onChange={handleChange}
+            className="input_field"
+          >
+         {Object.keys(exchangeRate).map(i=>(
+            <option key={i} value={i}>{i}</option>
+          ))}
+          </select>
+        </div>
+      </div>
+
+      <div className="output">
+        <h2>Converted Amount: {convertedAmount}</h2>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
